889d421b358fba7d27b8caca10d783bbd2faa520
vianney bailleux <vianneyba@free.fr>
2022-06-26 16:38:19 +0200

    add search params

--- crm/views.py
+++ crm/views.py
@@ -11,8 +11,23 @@ class ClientViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticated, permissions.ClientPermissions,)
     serializer_class = serializers.ClientSerializer
 
+
+
     def get_queryset(self):
         queryset = models.Client.objects.all()
+        if self.request.query_params.get('name') is not None:
+            name = self.request.query_params.get('name')
+            queryset = queryset.filter(last_name__icontains=name)
+        if self.request.query_params.get('email') is not None:
+            email = self.request.query_params.get('email')
+            queryset = queryset.filter(email__icontains=email)
+        if self.request.query_params.get('customer') is not None:
+            customer = self.request.query_params.get('customer')
+            queryset = queryset.filter(prospect=customer)
+        if self.request.query_params.get('vendor') is not None:
+            vendor = self.request.query_params.get('vendor')
+            queryset = queryset.filter(sales_contact__pk=vendor)
+
         return queryset
 
     def create(self, request, *args, **kwargs):
@@ -31,8 +46,39 @@ class ClientViewSet(viewsets.ModelViewSet):
 class ContractViewSet(viewsets.ModelViewSet):
 
     permission_classes = (IsAuthenticated, permissions.ContractPermissions,)
+
     def get_queryset(self):
         queryset = models.Contract.objects.all()
+
+        if self.request.query_params.get('client_id') is not None:
+            client = self.request.query_params.get('client_id')
+            queryset = queryset.filter(client__pk=client)
+        if self.request.query_params.get('client') is not None:
+            name = self.request.query_params.get('client')
+            queryset = queryset.filter(client__last_name__icontains=name)
+        if self.request.query_params.get('company') is not None:
+            company = self.request.query_params.get('company')
+            queryset = queryset.filter(client__company__icontains=company)
+        if self.request.query_params.get('email') is not None:
+            email = self.request.query_params.get('email')
+            queryset = queryset.filter(client__email__icontains=email)
+        if self.request.query_params.get('date') is not None:
+            date = self.request.query_params.get('date')
+            if date[0] == '>':
+                queryset = queryset.filter(date_created__gt=date[1:])
+            elif date[0] == '<':
+                queryset = queryset.filter(date_created__lte=date[1:])
+            else:
+                queryset = queryset.filter(date_created=date)
+        if self.request.query_params.get('amount') is not None:
+            amount = self.request.query_params.get('amount')
+            if amount[0] == '>':
+                queryset = queryset.filter(amount__gt=amount[1:])
+            elif amount[0] == '<':
+                queryset = queryset.filter(amount__lte=amount[1:])
+            else:
+                queryset = queryset.filter(amount=amount)
+
         return queryset
 
     def get_serializer_class(self):
@@ -57,7 +103,30 @@ class ContractViewSet(viewsets.ModelViewSet):
 class EventViewSet(viewsets.ModelViewSet):
 
     permission_classes = (IsAuthenticated, permissions.EventPermission,)
-    queryset = models.Event.objects.all()
+
+    def get_queryset(self):
+        queryset = models.Event.objects.all()
+
+        if self.request.query_params.get('email') is not None:
+            email = self.request.query_params.get('email')
+            queryset = queryset.filter(client__email__icontains=email)
+        if self.request.query_params.get('client_id') is not None:
+            client = self.request.query_params.get('client_id')
+            print(client)
+            queryset = queryset.filter(client__pk=client)
+        if self.request.query_params.get('client') is not None:
+            name = self.request.query_params.get('client')
+            queryset = queryset.filter(client__last_name__icontains=name)
+        if self.request.query_params.get('date') is not None:
+            date = self.request.query_params.get('date')
+            if date[0] == '>':
+                queryset = queryset.filter(event_date__date__gt=date[1:])
+            elif date[0] == '<':
+                queryset = queryset.filter(event_date__date__lte=date[1:])
+            else:
+                queryset = queryset.filter(event_date=date)
+
+        return queryset
 
     def get_serializer_class(self):
         if self.action == 'create':
